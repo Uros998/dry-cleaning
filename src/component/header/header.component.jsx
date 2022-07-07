1c6cc0ed36eb6bedfdf651691bc11d37f1c5a5f2
Uros98 <uros.milovanovic98@gmail.com>
2022-07-08 01:48:11 +0200

    Added navbar

---
+++ src/component/header/header.component.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./header.styles.scss";
+import {NavLink} from "react-router-dom";
+
+const Header = () => {
+
+    return (
+        <>
+            <input id="page-nav-toggle" className="main-navigation-toggle" type="checkbox"/>
+            <label htmlFor="page-nav-toggle">
+                <svg className="icon--menu-toggle" viewBox="0 0 60 30">
+                    <g className="icon-group">
+                        <g className="icon--menu">
+                            <path d="M 6 0 L 54 0"/>
+                            <path d="M 6 15 L 54 15"/>
+                            <path d="M 6 30 L 54 30"/>
+                        </g>
+                        <g className="icon--close">
+                            <path d="M 15 0 L 45 30"/>
+                            <path d="M 15 30 L 45 0"/>
+                        </g>
+                    </g>
+                </svg>
+            </label>
+            <nav className="main-navigation">
+                <ul>
+                    <li><NavLink activeClassName="selected" className='option' to='/'>Početna</NavLink></li>
+                    <li><NavLink activeClassName="selected" className='option' to='/usluga'>Usluge</NavLink></li>
+                    <li><NavLink activeClassName="selected" className='option' to='/about'>O nama</NavLink></li>
+                </ul>
+            </nav>
+        </>
+    )
+
+}
+
+
+export default Header;
---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./homepage.styles.scss";
+import Header from "../../component/header/header.component";
+
+const HomePage = () => {
+
+    return (
+        <>
+            <Header/>
+        </>
+    )
+}
+
+
+export default HomePage;
