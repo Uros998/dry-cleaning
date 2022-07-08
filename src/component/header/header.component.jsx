515e4aafb6bad11bc056dea308a1e969a87aeb8f
Uros98 <uros.milovanovic98@gmail.com>
2022-07-08 19:37:14 +0200

    Added responsive for nav and title

--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -1,34 +1,48 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./header.styles.scss";
 import {NavLink} from "react-router-dom";
 
 const Header = () => {
 
+    const [showScroll, setShowScroll] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            setShowScroll(window.scrollY >= 100);
+        })
+    }, []);
+
+
     return (
         <>
-            <input id="page-nav-toggle" className="main-navigation-toggle" type="checkbox"/>
-            <label htmlFor="page-nav-toggle">
-                <svg className="icon--menu-toggle" viewBox="0 0 60 30">
-                    <g className="icon-group">
-                        <g className="icon--menu">
-                            <path d="M 6 0 L 54 0"/>
-                            <path d="M 6 15 L 54 15"/>
-                            <path d="M 6 30 L 54 30"/>
+            <div className={showScroll ? "nav-black" : "nav-none"}>
+                <div className="logo">
+                    <h2>ETILEN</h2>
+                </div>
+                <input id="page-nav-toggle" className="main-navigation-toggle" type="checkbox"/>
+                <label htmlFor="page-nav-toggle">
+                    <svg className="icon--menu-toggle" viewBox="0 0 60 30">
+                        <g className="icon-group">
+                            <g className="icon--menu">
+                                <path d="M 6 0 L 54 0"/>
+                                <path d="M 6 15 L 54 15"/>
+                                <path d="M 6 30 L 54 30"/>
+                            </g>
+                            <g className="icon--close">
+                                <path d="M 15 0 L 45 30"/>
+                                <path d="M 15 30 L 45 0"/>
+                            </g>
                         </g>
-                        <g className="icon--close">
-                            <path d="M 15 0 L 45 30"/>
-                            <path d="M 15 30 L 45 0"/>
-                        </g>
-                    </g>
-                </svg>
-            </label>
-            <nav className="main-navigation">
-                <ul>
-                    <li><NavLink activeClassName="selected" className='option' to='/'>Početna</NavLink></li>
-                    <li><NavLink activeClassName="selected" className='option' to='/usluga'>Usluge</NavLink></li>
-                    <li><NavLink activeClassName="selected" className='option' to='/about'>O nama</NavLink></li>
-                </ul>
-            </nav>
+                    </svg>
+                </label>
+                <nav className="main-navigation">
+                    <ul>
+                        <li><NavLink activeClassName="selected" className='option' to='/'>Početna</NavLink></li>
+                        <li><NavLink activeClassName="selected" className='option' to='/usluga'>Usluge</NavLink></li>
+                        <li><NavLink activeClassName="selected" className='option' to='/about'>O nama</NavLink></li>
+                    </ul>
+                </nav>
+            </div>
         </>
     )
 
--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,28 +1,17 @@
 import React from "react";
 import "./homepage.styles.scss";
 import Header from "../../component/header/header.component";
-import BackgroundImg from "../../assets/homepage-background.jpg";
-import BackgroundImgV2 from "../../assets/homepage-background-v2.jpg";
 
 const HomePage = () => {
-
-
-    const style = {
-        backgroundImage: `url(${BackgroundImgV2})`,
-    }
-
 
     return (
         <>
             <Header/>
             <div className="homepage">
-                <div className="homepage-title-text" style={style}>
+                <div className="homepage-title-text" >
                     <div className="homepage-title">
-                        <h2>Hemijsko Čišćenje</h2>
-                    </div>
-                    <div className="homepage-text">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate harum iusto labore
-                            mollitia nostrum numquam quae sint temporibus. Dicta, praesentium.</p>
+                        <h2>ETILEN</h2>
+                        <p>Hemijsko Čišćenje</p>
                     </div>
                 </div>
             </div>
