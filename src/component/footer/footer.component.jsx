84cf6cb21c9f8ce09ba2ef6ab512931ab83d1b2f
Uros98 <uros.milovanovic98@gmail.com>
2022-07-10 16:34:48 +0200

    Added footer

---
+++ src/component/footer/footer.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./footer.styles.scss";
+
+
+
+const Footer = () => (
+    <div className='footer'>
+        <h2>Gde nas možete naći?</h2>
+        <p><span>Adresa:</span> Knjaževačka 25</p>
+        <p><span>Kontakt:</span> 061/1958484</p>
+    </div>
+)
+
+
+export default Footer;
