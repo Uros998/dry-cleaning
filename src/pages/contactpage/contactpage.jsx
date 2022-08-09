8d2c82b7eaea3f02f7ccfbc790431c5eb5904b9a
Uros98 <uros.milovanovic98@gmail.com>
2022-08-09 19:39:44 +0200

    Added maps on contact page

--- src/pages/contactpage/contactpage.jsx
+++ src/pages/contactpage/contactpage.jsx
@@ -51,6 +51,11 @@ const Contactpage = () => {
                             </div>
                         </div>
                     </div>
+                    <div className="contact-maps">
+                        <iframe
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2902.2769276564472!2d21.911692315769983!3d43.32939388132602!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc0b6ef9cfd252c26!2sHemijsko%20%C4%8Di%C5%A1%C4%87enje%20Slavica%20Etilen!5e0!3m2!1ssr!2srs!4v1660066228964!5m2!1ssr!2srs"
+    referrerPolicy="no-referrer-when-downgrade"/>
+                    </div>
                 </div>
             </div>
             <Footer/>
